0ded17d05e15894f35bcac011688afa632e3dc1f
Max <scorpiomax90@gmail.com>
2023-04-26 12:06:27 +0300

    separate nav-item

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from './NavItem.vue'
 
 const navItems = {
 	timeline: ClockIcon, 
@@ -12,16 +13,13 @@ const navItems = {
 <template>
 	<nav class="sticky bottom-0 z-10 bg-white">
 		<ul class="flex items-center justify-around border-t">
-
-			<li
-				v-for="(icon, item) in navItems"
-				:key="item"
-				class="flex-1"
+			<NavItem 
+			v-for="(icon, item) in navItems"
+			:key="item"
+			:href="`#${item}`"
 			>
-				<a class="flex flex-col items-center p-2 text-xs capitalize" :href="`#${item}`">
-					<component :is="icon" class="h-6 w-6" /> {{ item }}
-				</a>
-			</li>
+				<component :is="icon" class="h-6 w-6" /> {{ item }}
+			</NavItem>
 		</ul>
 	</nav>
 </template>

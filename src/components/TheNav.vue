586124f736c94f6e6f9d28c646924591c135eb4f
Max <scorpiomax90@gmail.com>
2023-04-27 09:08:00 +0300

    separate logic

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,17 +1,10 @@
 <script setup>
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constans'
+import { NAV_ITEMS } from '../constans'
 import NavItem from './NavItem.vue'
 
 defineProps(['currentPage'])
 
 const emit = defineEmits(['navigate'])
-
-const navItems = {
-	[PAGE_TIMELINE]: ClockIcon, 
-	[PAGE_ACTIVITIES]: ListBulletIcon, 
-	[PAGE_PROGRESS]: ChartBarIcon
-}
 
 </script>
 
@@ -19,7 +12,7 @@ const navItems = {
 	<nav class="sticky bottom-0 z-10 bg-white">
 		<ul class="flex items-center justify-around border-t">
 			<NavItem 
-			v-for="(icon, item) in navItems"
+			v-for="(icon, item) in NAV_ITEMS"
 			:key="item"
 			:href="`#${item}`"
 			:class="{ 'bg-gray-200 pointer-events-none': item === currentPage }"

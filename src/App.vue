49188be8cba63e0fd4eafb5806395bbc9b9e8a6e
Max <scorpiomax90@gmail.com>
2023-06-07 16:47:57 +0300

    Creating Activities

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,10 @@ function deleteActivity(activity) {
 	activities.value.splice(activities.value.indexOf(activity), 1)
 }
 
+function createActivity(activity) {
+	activities.value.push(activity)
+}
+
 </script>
 
 <template>
@@ -33,7 +37,7 @@ function deleteActivity(activity) {
   />
 	<main class="flex flex-grow flex-col">
 		<TheTimeLine v-show="currentPage === PAGE_TIMELINE" :timeline-items="timelineItems" :activity-select-options="activitySelectOptions" />
-		<TheActivities v-show="currentPage === PAGE_ACTIVITIES" :activities="activities" @delete-activity="deleteActivity" />
+		<TheActivities v-show="currentPage === PAGE_ACTIVITIES" :activities="activities" @delete-activity="deleteActivity" @create-activity="createActivity" />
 		<TheProgress v-show="currentPage === PAGE_PROGRESS" />
 	</main>
 	<TheNav :current-page="currentPage" @navigate="goTo($event)" />
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -9,7 +9,9 @@ defineProps({
 		type: Array
 	}
 })
-const emit = defineEmits(['deleteActivity'])
+const emit = defineEmits(['deleteActivity', 'createActivity'])
+
+let newActivity = ''
 </script>
 
 <template>
@@ -22,8 +24,14 @@ const emit = defineEmits(['deleteActivity'])
 				@delete="emit('deleteActivity', activity)"
 			/>
 		</ul>
-		<form class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
-			<input type="text" class="w-full rounded border px-4 text-xl" placeholder="Activity name">
+		<form @submit.prevent="emit('createActivity', newActivity)" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
+			<input
+			  :value="newActivity"
+				@input="newActivity = $event.target.value"
+			  class="w-full rounded border px-4 text-xl"
+			  placeholder="Activity name"
+			  type="text"
+			>
 			<BaseButton>
 				<PlusIcon  class="h-8 w-8" />
 			</BaseButton>

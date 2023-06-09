28ef7d1b9f4138248e46cd4ce2bf7ec88a8ef7f6
Max <scorpiomax90@gmail.com>
2023-06-09 16:37:53 +0300

    fix form activity

--- src/components/TheActivityForm.vue
+++ src/components/TheActivityForm.vue
@@ -1,26 +1,28 @@
 <script setup>
+import { ref } from 'vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
 import BaseButton from './BaseButton.vue'
 
 const emit = defineEmits(['submit'])
 
-let activity = ''
+const activity = ref('')
 
 function submit() {
-	emit('submit', activity)
+	emit('submit', activity.value)
+
+	activity.value = ''
 }
 </script>
 
 <template>
 	<form @submit.prevent="submit" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
 			<input
-			  :value="activity"
-				@input="activity = $event.target.value"
+				v-model="activity"
 			  class="w-full rounded border px-4 text-xl"
 			  placeholder="Activity name"
 			  type="text"
 			>
-			<BaseButton>
+			<BaseButton :disabled="activity.trim() === ''">
 				<PlusIcon  class="h-8 w-8" />
 			</BaseButton>
 		</form>

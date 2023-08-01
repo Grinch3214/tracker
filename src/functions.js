159a894c48fdb2486b0cec04d4740ed7f80c71d7
Max <scorpiomax90@gmail.com>
2023-08-01 13:46:37 +0300

    fix function file

--- src/functions.js
+++ src/functions.js
@@ -28,8 +28,6 @@ export function generatePeriodSelectOptions() {
     15, 30, 45, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480
   ]
 
-	console.log(periodsInMinutes)
-
   return periodsInMinutes.map((periodInMinutes) => ({
     value: periodInMinutes * SECONDS_IN_MINUTE,
     label: generatePeriodSelectOptionsLabel(periodInMinutes)

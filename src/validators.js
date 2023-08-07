1ecc50e22a50c25d75a4d45c0d5e13088375e672
Max <scorpiomax90@gmail.com>
2023-08-07 10:49:30 +0300

    refactors components x2

--- src/validators.js
+++ src/validators.js
@@ -1,7 +1,12 @@
 import { BUTTON_TYPES, NAV_ITEMS, HOURS_IN_DAY, MIDNIGHT_HOUR } from './constants'
+import { ICONS } from './icons'
 
 export function isPageValid(page) {
-  return NAV_ITEMS.some(navItem => navItem.page === page)
+  return NAV_ITEMS.some((navItem) => navItem.page === page)
+}
+
+export function isIconValid(icon) {
+  return Object.keys(ICONS).includes(icon)
 }
 
 export function isNavItemValid(navItem) {
@@ -29,14 +34,10 @@ export function isActivityValid({ id, name, secondsToComplete }) {
     return true
   }
 
-  return [
-    isNotEmptyString(id),
-    isNotEmptyString(name),
-    isNumber(secondsToComplete)
-  ].every(Boolean)
+  return [isNotEmptyString(id), isNotEmptyString(name), isNumber(secondsToComplete)].every(Boolean)
 }
 
-function isNotEmptyString(value) {
+export function isNotEmptyString(value) {
   return isString(value) && value.length > 0
 }
 
